bfde4d4aa00be198d88e76687bb807349625b592
Lev Kaplunov <limon4ik59rus@mail.ru>
2022-12-01 14:58:40 +0500

    Add target blank for links

--- utils/Html.js
+++ utils/Html.js
@@ -7,7 +7,7 @@ class Html {
   static toItalicString = (text) => Html.toTagString(text, 'i');
   static toStrikeString = (text) => Html.toTagString(text, 's');
   static toCodeString = (text) => Html.toTagString(text, 'code');
-  static toLinkText = (text, href) => `<a href="${href}">${text}</a>`;
+  static toLinkText = (text, href) => `<a target="_blank" href="${href}">${text}</a>`;
 }
 
 module.exports = Html;
