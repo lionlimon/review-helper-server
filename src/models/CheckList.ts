6bf053e30124e432c2aa81c47fea633adff40afb
Lev Kaplunov <limon4ik59rus@mail.ru>
2022-12-26 13:58:27 +0500

    Add formatting for list api

--- src/models/CheckList.ts
+++ src/models/CheckList.ts
@@ -1,4 +1,8 @@
 import { config } from 'dotenv';
+import {
+  PageObjectResponse,
+  TextRichTextItemResponse,
+} from '@notionhq/client/build/src/api-endpoints';
 import Notion from '../services/Notion';
 
 config();
@@ -6,6 +10,22 @@ config();
 const notion = new Notion(process.env.SECRET_TOKEN);
 
 export type CheckListListParams = { tag?: string };
+export type CheckListItem = Record<'id' | 'title' | 'subTask' | 'explainText', string>;
+export type CheckListResult = (PageObjectResponse & {
+  properties: {
+    Name: {
+      title: {text: {content: string}}[]
+    },
+
+    Message: {
+      rich_text: TextRichTextItemResponse[]
+    },
+
+    subtask: {
+      rich_text: TextRichTextItemResponse[]
+    }
+  }
+})[]
 
 export default class CheckList {
   static async list({ tag = '' }: CheckListListParams = {}) {
@@ -18,10 +38,19 @@ export default class CheckList {
 
     const { results } = await checkList.query();
 
-    return results;
+    return this._formatList(results as CheckListResult);
   }
 
   static tags() {
     return notion.getTagsOfDatabase(process.env.CHECK_LIST_DATABASE_ID);
+  }
+
+  static _formatList(data: CheckListResult) {
+    return data.map((item) => ({
+      id: item.id,
+      title: item.properties.Name.title[0].text.content,
+      subTask: item.properties.subtask.rich_text[0].text.content,
+      explainText: item.properties.Message.rich_text[0].text.content,
+    })) as CheckListItem[];
   }
 }
